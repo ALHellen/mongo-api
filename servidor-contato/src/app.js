7b05705c2de4d06afd40b25311ed4cf40039a063
Telcrika <telcrika31@gmail.com>
2019-11-21 20:09:32 -0300

    agora o bodyParser é global

--- servidor-contato/src/app.js
+++ servidor-contato/src/app.js
@@ -1,12 +1,17 @@
 const express = require("express")
 const app = express()
-
 const database = require('./model/database')
+const bodyParser = require("body-parser")
 database.connect()
+
 
 //rotas
 const index = require("./routes/index")
 const contatos = require("./routes/contatosRoute")
+
+//bodyParser global
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function (request, response, next) {
   response.header("Access-Control-Allow-Origin", "*")
